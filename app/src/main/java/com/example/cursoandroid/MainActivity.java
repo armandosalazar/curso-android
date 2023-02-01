4dc080fe603f9322f2cd663c419d3cf622108c73
Armando Salazar <armandosalazar351@gmail.com>
2023-01-31 22:19:27 -0600

    Add ViewsActivity and works

--- app/src/main/java/com/example/cursoandroid/MainActivity.java
+++ app/src/main/java/com/example/cursoandroid/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.buttonViews).setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, ViewsActivity.class));
-        });
+        findViewById(R.id.buttonViews).setOnClickListener(v -> startActivity(new Intent(this, ViewsActivity.class)));
     }
 }
